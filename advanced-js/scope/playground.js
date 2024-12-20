c86c985d4e7996ba95053c6ff954adec9193f75c
Jacob M <114640505+JMestre32@users.noreply.github.com>
2024-12-20 12:31:12 -0800

    Make scope notes

---
+++ advanced-js/scope/playground.js
@@ -0,0 +1,14 @@
+/*
+// NOT attached to the window
+const first = 'wes'; 
+// NOT attached to the window
+let second = "juno";
+// IS attached to the window
+var num = 100;
+
+It is generally not a good idea to make global variables!
+*/
+
+// function sayHi(){
+//     console.log("Hi!");
+// }

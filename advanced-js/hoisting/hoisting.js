1d00bad7b21eaf9061c8d566a6edefeeef1e73d2
Jacob M <114640505+JMestre32@users.noreply.github.com>
2024-12-20 13:29:35 -0800

    Finish hoisting

--- advanced-js/hoisting/hoisting.js
+++ advanced-js/hoisting/hoisting.js
@@ -1,1 +1,22 @@
-console.log("it works")
+sayHi();
+
+
+// The way js works is it takes all your function declarations and moves them to the top so they are all available for use before you use them!
+function sayHi(){
+    console.log('hey');
+    console.log(add(1, 2));
+}
+
+function add(a, b){
+    return a + b;
+}
+
+/* 
+
+Variable declarations are moved to the top, but the setting of those variables are not.
+
+console.log(num);
+const num = 10;
+*/
+
+// Hoisting is when variable and function declarations are 'hoisted' to the top of the file

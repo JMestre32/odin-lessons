262d957a7715bb8ace3047a9f93ec796fd6ca8fd
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2025-04-03 15:57:44 -0700

    IIFE notes

--- advanced-js/factory-functions-and-the-module-pattern/functions.js
+++ advanced-js/factory-functions-and-the-module-pattern/functions.js
@@ -15,3 +15,16 @@ const berry = createMan(24, "berry")
 const lebron = createMan(40, "LeBron")
 
 // We've created two separate objects without the need for a bunch of the same lines of code. Now if something goes wrong with either of the two, we know where to look. 
+
+const calculator = (function (){
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b
+    return{add, sub, mul, div};
+})();
+
+// This is an immediately invoked function expression or IIFE. It's an example of writing code, wrapping things away that we do not need as private variables/functions inside
+// our factory function. What's neat is while these things are tucked inside our module, we can use the returned variables/functions outside the factory, as necessary. 
+// Take for example add, there are multiple functions that may have the name add (i.e. adding two strings together or adding numbers together) Instead, of running into naming
+// collisions, IIFE's allow us to explicitly call calculator.add(a,b)

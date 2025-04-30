388b51ecd8fcf56a188d2d04a181a4eee68748e5
JMestre32 <jacobsaba32@gmail.com>
2025-04-30 16:47:51 -0700

    More notes on classes

--- advanced-js/Classes/classes.js
+++ advanced-js/Classes/classes.js
@@ -3,6 +3,7 @@
 /* 
 
     - Explain the differences between an object constructor and a class.
+    - Describe basic class syntax
     A: Classes are somewhat syntactic sugar. They were added in ES6 and are considered cleaner that constructor functions.
     Classes keep all of the code together. Pre-ES6, developers had to write more verbose code and methods had to be added manually.
 */
@@ -21,6 +22,25 @@ class Person {
   const p = new Person("Barry");
   p.greet();
 
+
+//   BASIC CLASS SYNTAX
+  class ClassName {
+    constructor(a, b, c) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  
+    method1() {
+      // your code here
+    }
+  
+    method2() {
+      // you can add more methods too
+    }
+  }
+  
+
 // Constructors
 function Person(name) {
     this.name = name;
@@ -35,11 +55,39 @@ function Person(name) {
   
 
 
+//   Explain what getters and setters are
 
+// Getters and setters are ways to access and update class properties in a controlled way
+class Person {
+    constructor(name) {
+      this._name = name;
+    }
   
+    get name() {
+      return this._name;
+    }
+  
+    set name(newName) {
+      if (newName.length < 2) {
+        console.log("Name too short!");
+      } else {
+        this._name = newName;
+      }
+    }
+  }
+  
+  const p = new Person("Barry");
+  p.name = "B";     // Name too short!
+  p.name = "Ben";   // Works
+  console.log(p.name); 
+  
+  
+
+
+
+
 /*
-    - Explain what getters and setters are.
-    - Describe basic class syntax.
+
     - Use inheritance with classes.
     - Explain how to implement private class fields and methods.
     - Explain what static properties and methods are.

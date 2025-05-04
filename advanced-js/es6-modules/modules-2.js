99b3b24a70887670a0f31b8587d91f680e69b815
JMestre32 <jacobsaba32@gmail.com>
2025-05-04 12:51:55 -0700

    More notes

--- advanced-js/es6-modules/modules-2.js
+++ advanced-js/es6-modules/modules-2.js
@@ -1,3 +1,8 @@
+//How to import: 
+
+import { greeting, farewell, greeting2, farewell2 } from "./modules.js";
+
+
 console.log(greeting)
 // The message will still appear in the console, despite greeting never being defined in this script. 
 // This is due to both files being loaded one after the other.
@@ -5,3 +10,6 @@ console.log(greeting)
 // What this implies is even if we use multiple .js files, they will all share the same global scope. 
 
 console.log(farewell)
+
+console.log(greeting2)
+console.log(farewell2)

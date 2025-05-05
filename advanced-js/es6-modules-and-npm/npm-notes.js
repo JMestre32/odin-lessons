b4dbf5d087c68f09a411913b45ed6e03976dbfe6
JMestre32 <jacobsaba32@gmail.com>
2025-05-05 15:21:52 -0700

    Just a few notes on npm

---
+++ advanced-js/es6-modules-and-npm/npm-notes.js
@@ -0,0 +1,10 @@
+// Short lil notes on npm
+
+/*
+npm is a package manager, it's a huge repository of plugins, libraries, and tools. npm provides us a command-line tool to install all of those things called "packages"
+in our applications. 
+
+
+npm revolvea around a file called package.json, the gist is that the file contains information such as dependencies and their versions and what npm does is read all of these and install
+all of it and run any commands we've set as an npm script (which will be covered later).
+*/

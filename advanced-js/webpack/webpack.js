b2d1d50cf2e5cae7c32bd9615d938a9faaede1b8
JMestre32 <jacobsaba32@gmail.com>
2025-05-06 14:33:12 -0700

    Start notes on webpack

---
+++ advanced-js/webpack/webpack.js
@@ -0,0 +1,10 @@
+// Learning objectives of this section
+
+/* 
+
+    - Explain the purpose of bundlers and how they work.
+    - Configure Webpack to bundle JavaScript modules.
+    - Configure Webpack to handle non-JavaScript files during bundling, including using HtmlWebpackPlugin.
+    - Set up Webpack’s development server.
+`
+*/
